149559d25ea54a79534b9a55c2e54779ee2f9331
farhad6366 <0325714958@student.wub.edu.bd>
2026-07-16 22:04:35 +0600

    terna

--- ternary.js
+++ ternary.js
@@ -4,12 +4,14 @@
 */
 // 
 let price = 500;
-const isLeader = false;
-if(isLeader){
+const isLeader = true;
+// if(isLeader){
 
-    price =0;
-}
-else{
-   price = price + 100; 
-}
-console.log('price is : ', price)
+//     price =0;
+// }
+// else{
+//    price = price + 100; 
+// }
+// console.log('price is : ', price)
+price = isLeader ? 0 : price +100;
+console.log('price now:', price);
