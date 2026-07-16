9d5f11a17f1cd9b738a52c96be1d73be357e0ace
farhad6366 <0325714958@student.wub.edu.bd>
2026-07-16 16:41:17 +0600

    vote.js push krchi

---
+++ vote.js
@@ -0,0 +1,11 @@
+const age =19
+const hasvoterIdCard = false;
+
+if(age>=18){
+    if(hasvoterIdCard){
+        console.log("tumi vote dite prbe");
+    }
+    else{
+        console.log("tumi vote dite prba na")
+    }
+}
