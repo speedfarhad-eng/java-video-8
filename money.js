86021e0c3ae277ef6ebd8d11cf2ee6b13a278d86
farhad6366 <0325714958@student.wub.edu.bd>
2026-07-16 16:25:11 +0600

    if e bitor kaj gula bar bar kaj krchi

---
+++ money.js
@@ -0,0 +1,13 @@
+const money = 50000;
+if(money  > 500){
+    console.log("mama tui  kintu botolok,amk kawa");
+    if(money> 1000){
+        console.log("tui ki teosion er beton paichos");
+        if(money> 5000){
+            console.log("tui amr apin bondhu")
+        }
+    }
+}
+else{
+    console.log("tui toh folirnni")
+}
