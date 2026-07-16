b90daee9e2146fa92fe6be3dfe9dc577c33340b1
farhad6366 <0325714958@student.wub.edu.bd>
2026-07-16 21:53:07 +0600

    tarmnary

---
+++ ternary.js
@@ -0,0 +1,15 @@
+/**
+ * binary -----> two
+ * ternary -----> three
+*/
+// 
+let price = 500;
+const isLeader = false;
+if(isLeader){
+
+    price =0;
+}
+else{
+   price = price + 100; 
+}
+console.log('price is : ', price)
